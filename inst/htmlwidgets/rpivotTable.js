f56bb24357a771e269c1834edd4dd26b0b2e6f1e
Enzo Martoglio <enzo@smartinsightsfromdata.com>
2015-10-27 16:26:01 +0000

    version 0.1.5.7

---
+++ inst/htmlwidgets/rpivotTable.js
@@ -0,0 +1,45 @@
+HTMLWidgets.widget({
+
+    name: 'rpivotTable',
+
+    type: 'output',
+
+    initialize: function(el, width, height) {
+
+	    return {};
+
+    },
+
+    renderValue: function(el, x, instance) {
+	    x.data = HTMLWidgets.dataframeToD3(x.data);
+
+	    var derivers = $.pivotUtilities.derivers;
+      var tpl = $.pivotUtilities.aggregatorTemplates;
+
+      x.params.renderers = $.extend(
+        $.pivotUtilities.renderers,
+        $.pivotUtilities.d3_renderers,
+        $.pivotUtilities.c3_renderers
+      );
+      
+      if (typeof x.params.sorters != "undefined") {
+        if (typeof x.params.sorters[0] == "string") {
+          x.params.sorters = eval("("+x.params.sorters[0]+")")
+        }
+      }
+      
+      if (typeof x.params.onRefresh != "undefined") {
+        x.params.onRefresh = x.params.onRefresh[0];
+      }
+      
+      $('#'+el.id).pivotUI(
+      		x.data, x.params, true
+      );
+
+    },
+
+    resize: function(el, width, height, instance) {
+
+    }
+
+});
